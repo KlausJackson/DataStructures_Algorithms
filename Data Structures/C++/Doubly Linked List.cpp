3e3c40c71390703a7f7b69f9c0c418d21e2d2a33
Klaus Jackson <klausjackson2@gmail.com>
2024-05-03 04:13:33 +0700

    changes

---
+++ Data Structures/C++/Doubly Linked List.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+class Node {
+	public:
+		int val;
+		Node* next;
+		
+		Node(int val) {
+			this->val = val;
+			next = NULL;
+		}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
